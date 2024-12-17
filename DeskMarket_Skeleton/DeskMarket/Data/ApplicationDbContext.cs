c5d4089f32c87a9acfeef3d073663de1547b98da
Georgi Hristov <georgi.hristov@bhtc.com>
2024-12-17 23:40:14 +0200

    Add db sets

--- DeskMarket_Skeleton/DeskMarket/Data/ApplicationDbContext.cs
+++ DeskMarket_Skeleton/DeskMarket/Data/ApplicationDbContext.cs
@@ -2,32 +2,31 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace DeskMarket.Data
+namespace DeskMarket.Data;
+
+public class ApplicationDbContext : IdentityDbContext
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+    }
 
-        public virtual DbSet<Product> Products { get; init; }
+    public virtual DbSet<Product> Products { get; set; }
 
-        public DbSet<Category> Categories { get; init; }
+    public DbSet<Category> Categories { get; set; }
 
-        public DbSet<ProductClient> ProductsClients { get; init; }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+    public DbSet<ProductClient> ProductsClients { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
 
-            //builder
-            //    .Entity<Category>()
-            //    .HasData(
-            //        new Category { Id = 1, Name = "Laptops" },
-            //        new Category { Id = 2, Name = "Workstations" },
-            //        new Category { Id = 3, Name = "Accessories" },
-            //        new Category { Id = 4, Name = "Desktops" },
-            //        new Category { Id = 5, Name = "Monitors" });
-        }
+        builder
+            .Entity<Category>()
+            .HasData(
+                new Category { Id = 1, Name = "Laptops" },
+                new Category { Id = 2, Name = "Workstations" },
+                new Category { Id = 3, Name = "Accessories" },
+                new Category { Id = 4, Name = "Desktops" },
+                new Category { Id = 5, Name = "Monitors" });
     }
 }
